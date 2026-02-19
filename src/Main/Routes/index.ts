ff9734ee9f22eee01d7133c63bc169d60c6b1a53
rafaelribeirowt <rafael.ribeirowt@gmail.com>
2026-02-19 16:51:07 -0300

    refactor template

---
+++ src/Main/Routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+// Importe e registre as rotas de cada modulo aqui:
+// import { usersRouter } from "./_module-name_/Routes";
+
+export const router = Router();
+
+// router.use("/users", usersRouter);
---
+++ src/Presentation/Contracts/HttpRequest.ts
@@ -0,0 +1,6 @@
+import { Request } from "express";
+
+export type AuthenticatedRequest = Request & {
+  userId?: string;
+  requestId?: string;
+};
